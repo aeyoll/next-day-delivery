d076025ea66ea46a0566a23582daeae9665e8fc7
Jean-Philippe Bidegain <jp@bidega.in>
2019-12-31 15:41:45 +0100

    Fix phpunit test in php 7.0

--- tests/NextDayDeliveryTest.php
+++ tests/NextDayDeliveryTest.php
@@ -7,7 +7,7 @@ use Aeyoll\NextDayDelivery;
 
 final class NextDayDeliveryTest extends TestCase
 {
-    public function testClassicWeekDayBeforeTimeLimit(): void
+    public function testClassicWeekDayBeforeTimeLimit()
     {
         $helper = new NextDayDelivery();
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-12-30 08:00:00')); // Monday before 3pm
@@ -15,7 +15,7 @@ final class NextDayDeliveryTest extends TestCase
         $this->assertEquals('07h00', $isNextDayDeliveryPossible);
     }
 
-    public function testClassicWeekDayAfterTimeLimit(): void
+    public function testClassicWeekDayAfterTimeLimit()
     {
         $helper = new NextDayDelivery();
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-12-30 16:00:00')); // Monday after 3pm
@@ -23,7 +23,7 @@ final class NextDayDeliveryTest extends TestCase
         $this->assertEquals(false, $isNextDayDeliveryPossible);
     }
 
-    public function testFridayBeforeTimeLimitIfSaturdayIsOk(): void
+    public function testFridayBeforeTimeLimitIfSaturdayIsOk()
     {
         $helper = new NextDayDelivery(['allowSaturdayDelivery' => true]);
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-12-13 08:00:00')); // Friday before 3pm
@@ -31,7 +31,7 @@ final class NextDayDeliveryTest extends TestCase
         $this->assertEquals('07h00', $isNextDayDeliveryPossible);
     }
 
-    public function testFridayBeforeTimeLimitIfSaturdayIsNotOk(): void
+    public function testFridayBeforeTimeLimitIfSaturdayIsNotOk()
     {
         $helper = new NextDayDelivery();
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-12-13 08:00:00')); // Friday before 3pm
@@ -39,7 +39,7 @@ final class NextDayDeliveryTest extends TestCase
         $this->assertEquals(false, $isNextDayDeliveryPossible);
     }
 
-    public function testWeekendDay(): void
+    public function testWeekendDay()
     {
         $helper = new NextDayDelivery();
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-12-14 16:00:00')); // Saturday
@@ -49,7 +49,7 @@ final class NextDayDeliveryTest extends TestCase
         $this->assertEquals(false, $isNextDayDeliveryPossible);
     }
 
-    public function testNextBusinessDayIsAHoliday(): void
+    public function testNextBusinessDayIsAHoliday()
     {
         $helper = new NextDayDelivery();
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-12-31 08:00:00')); // Tomorrow is a holiday
@@ -57,7 +57,7 @@ final class NextDayDeliveryTest extends TestCase
         $this->assertEquals(false, $isNextDayDeliveryPossible);
     }
 
-    public function testCurrentDayIsAHoliday(): void
+    public function testCurrentDayIsAHoliday()
     {
         $helper = new NextDayDelivery();
         $isNextDayDeliveryPossible = $helper->isNextDayDeliveryPossible(new \DateTime('2019-11-01 08:00:00')); // Tomorrow is a holiday
